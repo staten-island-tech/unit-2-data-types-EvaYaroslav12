f9bdacb6b9d7d415634b411f0e3d0b28cc47c080
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-02 09:26:07 -0500

    .

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 day_of_week = input("what day is it? ")
-if day_of_week == "Friday":
+input_lower = day_of_week.lower()
+if input_lower == "friday":
+    print("correct")
+elif input_lower == "day of fries":
     print("correct")
 else:
-    print("incorrect")
+    print("INCORRECT")
