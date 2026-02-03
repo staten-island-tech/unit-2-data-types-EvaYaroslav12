8284198840b1cf8f139904d01aea2bd785236404
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-03 08:49:51 -0500

    Update test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+imp = input("What's the weather? ")
+temp = int(imp)
+if temp >= 100:
+    print('Holy Dam')
+elif temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+elif temp <= -50:
+    print('Just a lil cold')
+else:
+    print('cold')
+
+print(IsEven(88))
