c91f46c4f2ac4144852f87a67793e70426ec2faa
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-22 22:48:20 -0400

    .

---
+++ quizy.py
@@ -0,0 +1,32 @@
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+
+# num = input('enter a NUMBAR: ')
+# numy = float(num)
+
+# for i in values:
+#     if numy == i: print('suceeeesss')
+
+
+# if numy in values:
+#     print ('suces')
+# if numy not in values:
+#     print ('fails :[')
+
+y = 0
+x = "this is a thing"
+#inputy = input('insert a letter: ')
+# for i in x:
+#     if i == inputy: y = y +1
+# print(y)
+
+for i in range(len(x)):
+    print(i, ' - ', x[i])
+ 
+ 
+ #  if (x[i] == inputy): y = y +1
+#print (y)
+#m = x.count(inputy)
+
